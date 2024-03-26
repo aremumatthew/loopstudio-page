99ba0f0977285a16d0150ee7cd5f1b574651463f
aremumatthew <aremumatthew112@gmail.com>
2024-03-26 22:53:43 +0000

    first commit to add all files

---
+++ index.js
@@ -0,0 +1,13 @@
+function openNav(){
+  document.querySelector(".sidebar-div").style.width = "100%";
+}
+function closeNav(){
+  document.querySelector(".sidebar-div").style.width = "0%";
+}
+
+function openmobileNav(){
+  document.querySelector(".sidebar-div").style.width = "100%";
+}
+function closemobileNav(){
+  document.querySelector(".sidebar-div").style.width = "0%";
+}
